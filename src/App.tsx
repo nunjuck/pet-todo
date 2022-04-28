07b2a23b6c0b3c3d0ab2a0e36f128b1617ac2978
Nikita Koshelev <nikita.koshelev@poleinvest.ru>
2022-04-28 20:11:23 +0300

    Update linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { ChakraProvider, theme, Container, Input } from '@chakra-ui/react';
+import { ChakraProvider, theme, Container, Input } from '@chakra-ui/react'
 
 export function App(): JSX.Element {
   return (
     <ChakraProvider theme={theme}>
       <Container>
-        <Input placeholder="Basic usage" />
+        <Input placeholder='Basic usage' />
       </Container>
     </ChakraProvider>
-  );
+  )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import * as React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
+import ReactDOM from 'react-dom'
+import * as React from 'react'
+
+import { App } from './App'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
