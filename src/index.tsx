c4c17d163d87124882e89f64e3345cf0bbcd2769
Nikita Koshelev <nikita.koshelev@poleinvest.ru>
2022-05-13 18:10:57 +0300

    update render root component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,7 @@
-import ReactDOM from 'react-dom'
-import * as React from 'react'
+import { createRoot } from 'react-dom/client'
 
 import { App } from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(<App />)
